6b473c075a91d9f0c46710d7802cc9b905a08a28
deepwidth <zhang.kangkang@outlook.com>
2020-02-13 23:16:22 +0800

    添加了部署模块的端口管理部分(portManager.php)

--- config.php
+++ config.php
@@ -17,6 +17,32 @@ define('__QUARKCO_ROOT_DIR__', dirname(__FILE__));
 //存放类的文件夹，在QuarkCO工作目录之下的目录，最后以'/'结束
 define('__CLASSES_ROOT_DIR__', 'QuarkCO/');
 
+@set_include_path(get_include_path() . PATH_SEPARATOR .
+__QUARKCO_ROOT_DIR__ . '/var');
+
 require_once "codeSync.php";
+
+require_once "portManager.php";
+
+//===============个性化服务设置==================
+
+//请在这里设置 QuarkCO 服务集所能使用端口范围的最小值
+$startPort = 2201;
+
+//请在这里设置 QuarkCO 服务集所能使用端口范围的最大值
+$endPort = 65535;
+
+/**
+ * 请在这里设置 QuarkCO 服务集不可以使用的端口，通常您会将一些其他应用可能会使用的
+ * 端口添加在下面
+ */
+$exceptedPorts = array(
+	22, 23, 80,	443, 1080, 3306, 25536
+);
+
+$portManager = PortManager::getInstance();
+$portManager->setMiniPort($startPort);
+$portManager->setMaxPort($endPort);
+$portManager->setExceptedPorts($exceptedPorts);
 
 ?>
---
+++ var/portManager.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * portManager.php
+ * Date: 2020.2.13
+ * Author: Zhang Kangkang
+ * Website: https://zkk.me
+ */
+
+class PortManager {
+	
+	private static $instance = null;
+
+	//最小端口是部署模块绑定服务的起始搜索端口
+	private $miniPort = 2201;
+
+	//最大端口是部署模块绑定服务的端口上界
+	private $maxPort = 65535;
+
+	//排除端口数组为不能用于 QuarkCO 绑定服务的端口
+	private $exceptedPorts = array();
+
+	private function __construct() {}
+
+	private function __clone() {}
+
+	public function  getInstance() {
+		if(self::$instance == null) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	public function setMiniPort($port) {
+		$this->miniPort = $port;
+	}
+
+	public function getMiniPort() {
+		return $this->miniPort;
+	}
+
+	public function setMaxPort($port) {
+		$this->maxPort = $port;
+	}
+
+	public function getMaxPort() {
+		return $this->maxPort;
+	}
+	
+	public function setExceptedPorts($ports) {
+		$this->exceptedPorts = $ports;
+	}
+
+	public function getExceptedPorts() {
+		return $this->exceptedPorts;
+	}
+
+	private function isAvailablePort($port) {
+		if(0 == strlen(shell_exec("lsof -i:$port")) && !in_array($port, $this->exceptedPorts)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public function findAvailablePort() {
+		for($port = $this->miniPort; $port <= $this->maxPort; ++$port) {
+			if($this->isAvailablePort($port)) {
+				return $port;
+			}
+		}
+		return -1;
+	}
+}
