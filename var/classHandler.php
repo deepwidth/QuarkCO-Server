f57600ed0d76c47bb706c78f486f3ed856e93859
zhangkangkang <zhang.kangkang@outlook.com>
2020-02-14 20:47:53 +0800

    添加了Java文件处理类(classHandler.php)

---
+++ var/classHandler.php
@@ -0,0 +1,17 @@
+<?php
+	
+/**
+ * classHandler.php
+ * Date: 2020.2.14
+ * Author: Zhang Kangkang
+ * Website: https://zkk.me
+ */
+ 
+class ClassHandler {
+	
+	private $classFullName;	//类完整名字，包括包名
+	private $classFilePath;	//文件路径
+	private $classContent;	//类代码内容
+}
+ 
+?>
